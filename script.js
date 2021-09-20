31f4770be8f0e2c2fe69a6a71b17f40ae7eb9fc5
iikim <iikim2511@gmail.com>
2021-09-19 22:49:56 -0400

    js 02 "navigation-button

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// js 02 "navigation-button
+
+
+const navBtn = document.querySelectorAll('.navigation-button');
+
+Array.from(navBtn).forEach((a_item)=>{
+
+    a_item.addEventListener('click',()=>{
+        a_item.parentElement.parentElement.classList.toggle('chage');
+    });
+
+});
