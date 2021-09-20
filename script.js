465215fff2f0fba692cc53e15f495a1f316b253b
iikim <iikim2511@gmail.com>
2021-09-20 11:25:21 -0400

    // scrollBy()

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ Array.from(navBtn).forEach((a_item)=>{
     });
 
 });
+
+
+// ❌코딩용... 자동으로 가장 밑으로 스크롤
+// scrollBy()
+document.addEventListener("scroll",()=>{
+    window.scrollBy(0, window.innerHeight);
+});
