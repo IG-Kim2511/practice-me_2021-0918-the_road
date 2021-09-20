99a1f008518de4bc11e9c9046cff7ef1fb7d9b5e
iikim <iikim2511@gmail.com>
2021-09-20 19:32:39 -0400

    /* js 04 put colors */

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ Array.from(navBtn).forEach((a_item)=>{
     window.scrollBy(0, window.innerHeight);
 });
  */
+
+/* js 04 put colors */
+const color = ['red','blue','green','yellow','orange']
+
+let i = 0;
+const navLink = document.querySelectorAll('.nav-link');
+
+Array.from(navLink).forEach((a_item)=>{
+    a_item.style.cssText = ``
+});
+
+/* js 06 click nav  icon  */
+
+const container = document.querySelector('.container');
+
+const openNavbar = document.querySelector(".open-navbar-icon");
+const closeNavbar = document.querySelector('.close-navbar-icon');
+
+openNavbar.addEventListener("click",()=>{
+    container.classList.add('change');
+});
+closeNavbar.addEventListener('click',()=>{
+    container.classList.remove('change');
+});
