8791f4eee6a9baa5613ad7bc5e22776408abf2bb
iikim <iikim2511@gmail.com>
2021-09-19 22:52:12 -0400

    (js 02: fix)

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const navBtn = document.querySelectorAll('.navigation-button');
 Array.from(navBtn).forEach((a_item)=>{
 
     a_item.addEventListener('click',()=>{
-        a_item.parentElement.parentElement.classList.toggle('chage');
+        a_item.parentElement.parentElement.classList.toggle('change');
     });
 
 });
